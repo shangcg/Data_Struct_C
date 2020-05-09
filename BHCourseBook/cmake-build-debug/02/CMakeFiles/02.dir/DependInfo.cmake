99073d6b78737c6be11ce613cbcf517834660908
shangchenguang <1994.shang01>
2020-05-09 16:01:32 +0800

    创建BH目录

---
+++ BHCourseBook/cmake-build-debug/02/CMakeFiles/02.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/shangchenguang/Documents/studywork/Data-Structure/CLion/BHCourseBook/02/P2.c" "/Users/shangchenguang/Documents/studywork/Data-Structure/CLion/BHCourseBook/cmake-build-debug/02/CMakeFiles/02.dir/P2.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Status"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ BHCourseBook/cmake-build-debug/02/CMakeFiles/02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "02"
+  "02.pdb"
+  "CMakeFiles/02.dir/P2.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
